f54e496afbfe6e2e45186ccccfe86b40f4a953ab
Mike Pennisi <mike@mikepennisi.com>
2013-09-10 16:46:56 -0400

    regenerate api docs

--- api-docs/api.js
+++ api-docs/api.js
@@ -1,15 +1,6 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
-    "classes": [
-        "Marionette.Actions",
-        "Marionette.Client",
-        "Marionette.CommandStream",
-        "Marionette.Drivers.Abstract",
-        "Marionette.Drivers.MozTcp",
-        "Marionette.Drivers.Tcp",
-        "Marionette.Element",
-        "Marionette.MultiActions"
-    ],
+    "classes": [],
     "modules": [],
     "allModules": []
 } };
